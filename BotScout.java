eaca507f5b7db96f46265cec045d1e96f784d12b
Jiří Setnička <setnicka@seznam.cz>
2016-03-15 22:04:24 +0100

    Scouti: Čekání na zombíky cestou
    Pokud už scout nabere nějakého zombíka a hrizí, že by mu utekl (zombík
    už je moc daleko a dalším pohybem by se ještě vzdálil), tak k pohybu
    nedojde a počká se na opozdilého zombíka.

--- BotScout.java
+++ BotScout.java
@@ -5,6 +5,7 @@ import battlecode.common.*;
 public class BotScout extends Bot {
 
 	MapLocation target;
+	int ZOMBIE_DISTANCE_LIMIT = 16;
 
 	public BotScout(RobotController rc) {
 		super(rc);
@@ -26,6 +27,18 @@ public class BotScout extends Bot {
 			e.printStackTrace();
 		}
 
+	}
+
+	// pokud je zombie dal nez nastaveny limit a nasim pohybem bychom ji utekli jeste vic,
+	// tak se rozhodneme nepohnout
+	// idea je nesetrast ziskane zombiky cestou a dovest je do cile
+	public void moveIfZombieNear(RobotInfo nearest, Direction dir) throws Exception {
+		if (nearest != null) {
+			int distance = nearest.location.distanceSquaredTo(rc.getLocation());
+			int new_distance = nearest.location.distanceSquaredTo(rc.getLocation().add(dir));
+			if (distance > ZOMBIE_DISTANCE_LIMIT && new_distance > distance) return;
+		}
+		rc.move(dir);
 	}
 
 	public void loop() {
@@ -54,14 +67,27 @@ public class BotScout extends Bot {
 					dir_2 = dir_1.rotateLeft();
 					dir_3 = dir_1.rotateRight();
 
+					// podivame se, jaka je nejblizsi zombie
+					RobotInfo nearest = null;
+					for (RobotInfo robot: rc.senseNearbyRobots()) {
+						if (robot.team != Team.ZOMBIE) continue;
+						if (nearest == null) nearest = robot;
+						int distance_nearest = nearest.location.distanceSquaredTo(rc.getLocation());
+						int distance = robot.location.distanceSquaredTo(rc.getLocation());
+						if (distance < distance_nearest) nearest = robot;
+					}
+
 					// zkus se pohnout jednim z techto smeru, pokud to nelze
 					// kvuli rubble, odstran ho, jinak zvol nahodny smer
 					if (rc.canMove(dir_1)) {
-						rc.move(dir_1);
+						//rc.move(dir_1);
+						moveIfZombieNear(nearest, dir_1);
 					} else if (rc.canMove(dir_2)) {
-						rc.move(dir_2);
+						//rc.move(dir_2);
+						moveIfZombieNear(nearest, dir_2);
 					} else if (rc.canMove(dir_3)) {
-						rc.move(dir_3);
+						//rc.move(dir_3);
+						moveIfZombieNear(nearest, dir_3);
 					} else if (rc.senseRubble(rc.getLocation().add(dir_1)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
 						rc.clearRubble(dir_1);
 					} else if (rc.senseRubble(rc.getLocation().add(dir_2)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
