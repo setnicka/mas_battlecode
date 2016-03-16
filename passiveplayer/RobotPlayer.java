671587e2835e8d1d22a0d328d8d9a8d6de61356e
Michal Filippi <michal.filippi@gmail.com>
2016-03-16 20:14:38 +0100

    Pridani pasivniho hrace.

---
+++ passiveplayer/RobotPlayer.java
@@ -0,0 +1,12 @@
+package passiveplayer;
+
+import battlecode.common.*;
+
+public class RobotPlayer {
+    public static void run(RobotController rc) {
+    	// Pasivni hrac. Pouze pro testovaci ucely.
+    	
+        while (true) {
+        }
+    }
+}
